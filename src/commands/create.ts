a794773cf99bee73e0663580a07482bc02276309
itschip <onlygamernerd@gmail.com>
2020-11-22 19:36:34 +0100

    del(cli): removed unused imports for now

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -1,10 +1,9 @@
-import figlet from "figlet";
-import chalk from "chalk";
 import CreateLanguage from '../prompts/createLanguage'
 import CreateResource from '../prompts/createResource';
+import CreatePackages from '../prompts/createPackages';
 
 export const createCommand = async () => {
   const language = await CreateLanguage()
   const resourceName = await CreateResource()
-  // const plugins = await CreatePackages(language, resourceName)
+  CreatePackages(language, resourceName)
 }
