e9f36603485e93365aa4eb956dcf174d1c871f3e
Christopher <gjeltendigital@gmail.com>
2020-11-23 01:21:06 +0100

    feat(cli/tsresource): added spinners | creation of folder and files + packages

--- src/functions/createTypescriptResource.ts
+++ src/functions/createTypescriptResource.ts
@@ -1,20 +1,37 @@
 import fs from 'fs';
 import path from 'path';
+import shell from 'shelljs';
+import ora from 'ora';
 
 const createTypescriptResource = (resourceName: string) => {
-  const manifestPath = path.resolve(resourceName)
-  let data = "fx_version 'adamant'\ngame 'gta5' \n\nclient_script '*.client.js'\n\nserver_script '*.server.js'"
 
+  const manifestPath = path.resolve(resourceName)
+  const data = "fx_version 'adamant'\ngame 'gta5' \n\nclient_script '*.client.js'\n\nserver_script '*.server.js'"
+  const execString = `cd ${manifestPath} && yarn init -y --silent && yarn add @citizenfx/client @citizenfx/server --silent`
+
+  const spinner = ora(`Creating ${resourceName} folder`).start();
+  fs.mkdirSync(manifestPath);
+  spinner.succeed();
+
+  spinner.text = "Creating fxmanifest.lua"
   fs.writeFile(`${manifestPath}/fxmanifest.lua`, data, (err) => {
     if (err) {
-      console.log(err)
+      spinner.fail()
     } else {
-      console.log('Created fxmanifest')
+      spinner.succeed();
     }
   })
+  spinner.succeed();
 
-  console.log("typescript")
+  spinner.text = "Adding packages"
+  if (shell.exec(execString).code !== 0) {
+    spinner.fail();
+    shell.exit(1)
+  }
+
+
 }
+
 
 export default createTypescriptResource
 
