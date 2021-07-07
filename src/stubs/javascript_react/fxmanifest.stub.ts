bfe0dde25246c2541c57e5ba34fd580961a89a64
Christopher <onlygamernerd@gmail.com>
2021-07-07 16:47:15 +0200

    feat(stubs): react fxmanifest

---
+++ src/stubs/javascript_react/fxmanifest.stub.ts
@@ -0,0 +1,17 @@
+export const fxmanifest = `fx_version 'cerulean'
+
+game 'gta5'
+
+name '%s'
+
+client_script 'client/client.js'
+
+server_script 'server/server.js'
+
+ui_page 'html/index.html'
+
+files {
+  'html/index.html',
+  'html/index.css',
+  'html/index.js'
+}`
---
+++ src/stubs/typescript_react/fxmanifest.stub.ts
@@ -0,0 +1,17 @@
+export const fxmanifest = `fx_version 'cerulean'
+
+game 'gta5'
+
+name '%s'
+
+client_script 'dist/client/*.client.js'
+
+server_script 'dist/server/*.server.js'
+
+ui_page 'html/index.html'
+
+files {
+  'html/index.html',
+  'html/index.css',
+  'html/index.js'
+}`
