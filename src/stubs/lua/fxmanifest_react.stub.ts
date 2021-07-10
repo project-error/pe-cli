9744b0cbe1d09e30d2373b9137dd4ae54506238b
Chirstopher Gjelten <onlygamernerd@gmail.com>
2021-07-10 17:01:31 +0200

    feat(stubs): react stub for Lua

---
+++ src/stubs/lua/fxmanifest_react.stub.ts
@@ -0,0 +1,17 @@
+export const fxmanifest = `fx_version 'cerulean'
+
+game 'gta5'
+
+name '%s'
+
+client_script 'client/client.lua'
+
+server_script 'server/server.lua'
+
+ui_page 'html/index.html'
+
+files {
+  'html/index.html',
+  'html/index.css',
+  'html/index.js'
+}`;
