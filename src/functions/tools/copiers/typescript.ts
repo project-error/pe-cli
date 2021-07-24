e9dab061b3258064952ab74f5b7144f0676bb73c
Christopher <onlygamernerd@gmail.com>
2021-07-24 21:51:53 +0200

    few fixes on react boilerpalte creation

--- src/functions/tools/copiers/typescript.ts
+++ src/functions/tools/copiers/typescript.ts
@@ -2,22 +2,22 @@ import fs from 'fs';
 import { copySync } from 'fs-extra';
 
 export const copyFiles = (resourcePath: string, uiFramework: string): void => {
-  fs.copyFileSync(`${resourcePath}/cfa-templates/ts/package.json`, `${resourcePath}/package.json`);
-  fs.copyFileSync(
-    `${resourcePath}/cfa-templates/ts/webpack.config.js`,
-    `${resourcePath}/webpack.config.js`,
-  );
-  // tsconfig
-  fs.copyFileSync(
-    `${resourcePath}/cfa-templates/ts/client/tsconfig.json`,
-    `${resourcePath}/client/tsconfig.json`,
-  );
-  fs.copyFileSync(
-    `${resourcePath}/cfa-templates/ts/server/tsconfig.json`,
-    `${resourcePath}/server/tsconfig.json`,
-  );
+	fs.copyFileSync(`${resourcePath}/cfa-templates/ts/package.json`, `${uiFramework !== 'none' ? resourcePath : `${resourcePath}/resources`}/package.json`);
+	fs.copyFileSync(
+		`${resourcePath}/cfa-templates/ts/webpack.config.js`,
+		`${resourcePath}/webpack.config.js`,
+	);
+	// tsconfig
+	fs.copyFileSync(
+		`${resourcePath}/cfa-templates/ts/client/tsconfig.json`,
+		`${resourcePath}/client/tsconfig.json`,
+	);
+	fs.copyFileSync(
+		`${resourcePath}/cfa-templates/ts/server/tsconfig.json`,
+		`${resourcePath}/server/tsconfig.json`,
+	);
 
-  if (uiFramework !== 'none') {
-    copySync(`${resourcePath}/cfa-templates/${uiFramework}`, `${resourcePath}/ui`);
-  }
+	if (uiFramework !== 'none') {
+		copySync(`${resourcePath}/cfa-templates/${uiFramework}`, `${resourcePath}/ui`);
+	}
 };
--- src/functions/tools/essentials.ts
+++ src/functions/tools/essentials.ts
@@ -4,53 +4,59 @@ import { sprintf } from 'sprintf-js';
 import { supportedLanguage } from '../../types/index';
 
 export const createEssentials = async (
-  resourcePath: string,
-  resourceName: string,
-  language: supportedLanguage,
-  uiFramework: string,
+	resourcePath: string,
+	resourceName: string,
+	language: supportedLanguage,
+	uiFramework: string,
 ) => {
-  const spinner = ora('Loading universe!').start();
-  try {
-    spinner.text = `Creating ${resourceName} resource!`;
-    // Creating the folder
-    fs.mkdirSync(resourcePath);
-    spinner.succeed('Successfully created resource path!');
-  } catch (error) {
-    spinner.fail('Failed to create resource path!');
-    return;
-  }
+	const spinner = ora('Loading universe!').start();
+	try {
+		spinner.text = `Creating ${resourceName} resource!`;
+		// Creating the folder
+		fs.mkdirSync(resourcePath);
+		spinner.succeed('Successfully created resource path!');
+	} catch (error) {
+		spinner.fail('Failed to create resource path!');
+		return;
+	}
 
-  spinner.text = 'Creating fxmanifest!';
-  try {
-    if (uiFramework !== 'none') {
-      const { fxmanifest } = await import(`../../stubs/${language}/fxmanifest_react.stub`);
-      fs.writeFileSync(`${resourcePath}/fxmanifest.lua`, sprintf(fxmanifest, resourceName));
-    } else {
-      const { fxmanifest } = await import(`../../stubs/${language}/fxmanifest.stub`);
-      fs.writeFileSync(`${resourcePath}/fxmanifest.lua`, sprintf(fxmanifest, resourceName));
-    }
+	spinner.text = 'Creating fxmanifest!';
+	try {
+		if (uiFramework !== 'none') {
+			const { fxmanifest } = await import(`../../stubs/${language}/fxmanifest_react.stub`);
+			fs.writeFileSync(`${resourcePath}/fxmanifest.lua`, sprintf(fxmanifest, resourceName));
+		} else {
+			const { fxmanifest } = await import(`../../stubs/${language}/fxmanifest.stub`);
+			fs.writeFileSync(`${resourcePath}/fxmanifest.lua`, sprintf(fxmanifest, resourceName));
+		}
 
-    spinner.succeed('Successfully created fxmanifest.lua');
-  } catch (error) {
-    spinner.fail('Failed to create fxmanifest!');
-    return;
-  }
+		spinner.succeed('Successfully created fxmanifest.lua');
+	} catch (error) {
+		spinner.fail('Failed to create fxmanifest!');
+		return;
+	}
 
-  spinner.text = 'Creating client and server files!';
-  // CREATING
-  try {
-    fs.mkdirSync(`${resourcePath}/client`);
-    fs.mkdirSync(`${resourcePath}/server`);
+	spinner.text = 'Creating client and server files!';
+	try {
+		if (uiFramework !== 'none' && language !== 'Lua') {
+			fs.mkdirSync(`${resourcePath}/resources/client`);
+			fs.mkdirSync(`${resourcePath}/resources/server`);
 
-    if (uiFramework !== 'none') fs.mkdirSync(`${resourcePath}/ui`);
+		} else {
+			fs.mkdirSync(`${resourcePath}/client`);
+			fs.mkdirSync(`${resourcePath}/server`);
+		}
 
-    const { createFiles } = await import(`./creators/${language}`);
 
-    createFiles(resourcePath);
+		if (uiFramework !== 'none') fs.mkdirSync(`${resourcePath}/ui`);
 
-    spinner.succeed('Successfully created essential files!');
-  } catch (error) {
-    spinner.fail('Failed to create essential files!');
-    return;
-  }
+		const { createFiles } = await import(`./creators/${language}`);
+
+		createFiles(resourcePath);
+
+		spinner.succeed('Successfully created essential files!');
+	} catch (error) {
+		spinner.fail('Failed to create essential files!');
+		return;
+	}
 };
